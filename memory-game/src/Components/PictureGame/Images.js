7432f2e7ed75d8852efe1c76601ef109702f4b1c
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-06-09 21:46:10 +0200

    Feature: Images Component is now displaying 4x4 pictures in a row with the map function

--- memory-game/src/Components/PictureGame/Images.js
+++ memory-game/src/Components/PictureGame/Images.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import pictureData from '../../data';
+import styled from "styled-components";
+
+const StyledDiv = styled.div`
+
+img {
+        width: 20%;
+        height: 6rem;
+        margin: 1rem;
+}`;
 
 
 const Images = () => {
         return ( 
                 pictureData.map((image, index) => {
                         return(
-                                <div key={index}>
+                                <StyledDiv key={index}>
                                         <img src={image.img} alt="memory game" />
-                                </div>
+                                        <img src={image.img2} alt="memory game" />
+                                        <img src={image.img3} alt="memory game" />
+                                        <img src={image.img4} alt="memory game" />
+                                </StyledDiv>
                         )
                 })
          );
--- memory-game/src/data.js
+++ memory-game/src/data.js
@@ -1,56 +1,34 @@
 const pictureData = [
         {
-                img: "./Pictures/bulb.jpg"
+                img: "./Pictures/bulb.jpg",
+                img2: "./Pictures/chess.jpg",
+                img3: "./Pictures/chip.jpg",
+                img4: "./Pictures/water.jpg"
+
         },
+
         {
-                img: "./Pictures/chess.jpg"
+                img: "./Pictures/chip.jpg",
+                img2: "./Pictures/colorfire.jpg",
+                img3: "./Pictures/earth.jpg",
+                img4: "./Pictures/fire.jpg"
+        },
+
+        {
+                img: "./Pictures/fire.jpg",
+                img2: "./Pictures/foots.jpg",
+                img3: "./Pictures/haribo.jpg",
+                img4: "./Pictures/head.jpg"
+
 
         },
         {
-                img: "./Pictures/chip.jpg"
+                img: "./Pictures/monkey.jpg",
+                img2: "../../Pictures/waterfall.jpg",
+                img3: "./Pictures/rubrik.jpg",
+                img4: "./Pictures/think.jpg"
 
         },
-        {
-                img: "./Pictures/colorfire.jpg"
-        },
-        {
-                img: "./Pictures/earth.jpg"
-
-        },
-        {
-                img: "./Pictures/fire.jpg"
-        },
-        {
-                img: "./Pictures/fire.jpg"
-        },
-        {
-                img: "./Pictures/foots.jpg"
-        },
-        {
-                img: "./Pictures/haribo.jpg"
-        },
-        {
-                img: "./Pictures/head.jpg"
-        },
-        {
-                img: "./Pictures/monkey.jpg"
-        },
-        {
-                img: "./Pictures/planet.jpg"
-        },
-        {
-                img: "./Pictures/rubrik.jpg"
-        },
-        {
-                img: "./Pictures/think.jpg"
-
-        },
-        {
-                img: "../../Pictures/water.jpg"
-        },
-        {
-                img: "../../Pictures/waterfall.jpg"
-        }
 ]
 
 export default pictureData;
