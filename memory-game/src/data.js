9869afcaf802e1e0545055cae1448280ea613f95
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-06-09 21:11:33 +0200

    Feature: new Data.js

---
+++ memory-game/src/data.js
@@ -0,0 +1,54 @@
+export const cardData = [
+        {
+                img: "./pictures/bulb.jpg"
+        },
+        {
+                img: "./pictures/chess.jpg"
+
+        },
+        {
+                img: "./pictures/chip.jpg"
+
+        },
+        {
+                img: "./pictures/colorfire.jpg"
+        },
+        {
+                img: "./pictures/earth.jpg"
+
+        },
+        {
+                img: "./pictures/fire.jpg"
+        },
+        {
+                img: "./pictures/fire.jpg"
+        },
+        {
+                img: "./pictures/foots.jpg"
+        },
+        {
+                img: "./pictures/haribo.jpg"
+        },
+        {
+                img: "./pictures/head.jpg"
+        },
+        {
+                img: "./pictures/monkey.jpg"
+        },
+        {
+                img: "./pictures/planet.jpg"
+        },
+        {
+                img: "./pictures/rubrik.jpg"
+        },
+        {
+                img: "./pictures/think.jpg"
+
+        },
+        {
+                img: "./pictures/water.jpg"
+        },
+        {
+                img: "./pictures/waterfall.jpg"
+        }
+]
