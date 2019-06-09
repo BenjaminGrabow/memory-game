7089cc65490d770893fd8e2767b732410463bce3
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-06-09 20:41:50 +0200

    Feature: styling for Header Component

--- memory-game/src/Components/Header/Header.js
+++ memory-game/src/Components/Header/Header.js
@@ -1,15 +1,27 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const StyledHeader = styled.header`
+width: 100%;
+
+a {
+        width: 100%;
+        text-decoration: none;
+        margin: 1rem;
+}
+
+`;
 
 const Header = () => {
         return (
-                <header>
+                <StyledHeader>
                         <nav>
                                 <a href="#">Home</a>
                                 <a href="#">About</a>
                                 <a href="#">Games</a>
                                 <a href="#">Team</a>
                         </nav>
-                </header>
+                </StyledHeader>
           );
 }
  
