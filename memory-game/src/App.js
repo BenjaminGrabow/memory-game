27cabae04ab4cc6288e8159ec0365a22541e9d22
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-06-09 20:31:33 +0200

    Feature: new Component Container

--- memory-game/src/App.js
+++ memory-game/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Container from './Components/Container/Container';
 
 function App() {
   return (
     <div className="App">
-     
+     <Container></Container>
     </div>
   );
 }
