ac4d3606dc834e75c3a52d41122160b43f06dec6
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-06-09 20:26:14 +0200

    Feature: Header Component

---
+++ memory-game/src/Components/Header/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Header = () => {
+        return (
+                <header>
+                        <nav>
+                                <a href="#">Home</a>
+                                <a href="#">About</a>
+                                <a href="#">Games</a>
+                                <a href="#">Team</a>
+                        </nav>
+                </header>
+          );
+}
+ 
+export default Header;
